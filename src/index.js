0f8878f33640c40f64f022524fc6f3572c8099fb
luis <foreverluiscastro@gmail.com>
2021-03-26 19:18:43 -0400

    Completed project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  document.getElementById('create-task-form').addEventListener('submit', addTask)
+})
+
+function addTask(event) {
+  event.preventDefault()
+  let input = event.target.querySelector('input').value
+  const container = document.querySelector('#tasks')
+  const liTag = document.createElement('li')
+  liTag.textContent = input
+  container.appendChild(liTag)
+  event.target.querySelector('input').value = ""
+}
